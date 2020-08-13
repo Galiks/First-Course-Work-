0c578e5a6ab6c958712757061618647c240da467
Galiks <31892748+Galiks@users.noreply.github.com>
2020-08-14 03:36:10 +0400

    Добавил возможность изменять цвет фона на странице с добавление ссылок. Подкорректировал footer

---
+++ WebApplication1/wwwroot/js/changeBackground.js
@@ -0,0 +1,6 @@
+﻿$('#changeColorButton').click(function () {
+    var color = $('#changeColorSelect').val();
+    console.log(color)
+    $('body').css("background-color", color);
+    localStorage.setItem('background', color);
+});
